7a3a0413011772fd40e4148894ee3f0edc6b9989
camilorendon <juancamilo.g.0512@gmail.com>
2022-08-25 21:07:22 -0500

    logica del proceso de carga y filtro

--- portafolio/src/app/services/productos.service.ts
+++ portafolio/src/app/services/productos.service.ts
@@ -12,6 +12,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [] ;
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) { 
 
@@ -19,16 +20,58 @@ export class ProductosService {
   }
 
 
+
   private cargarProductos(){
+
+    return new Promise((resolve, reject)=>{
       this.http.get ('https://angular-html-53673-default-rtdb.firebaseio.com/productos_idx.json')
       .subscribe((resp: any)=>{
         this.productos = resp;
-        this.cargando = false;
+        this.cargando = false; 
+        resolve;
       });
+    })
+    
+      
   }
 
   getProducto(id: string){
      return this.http.get(`https://angular-html-53673-default-rtdb.firebaseio.com/productos/${id}.json`);
+
+  }
+
+  buscarProducto(termino: string){
+    if(this.productos.length === 0){
+      //cargar productos
+      this.cargarProductos().then(()=>{
+        //ejecutar despues de tener los productos
+        //aplicar filtro
+        this.filtrarProductos(termino);
+      });
+    }else{
+      //aplicar filtro
+      this.filtrarProductos(termino);
+    }
+
+
+
+   
+  }
+
+  private filtrarProductos(termino: string){
+    //console.log(this.productos);
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach(prod =>{
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if(prod.categoria.indexOf(termino) >=0 || tituloLower.indexOf(termino) >= 0 ){
+        this.productosFiltrado.push(prod);
+      }
+
+    });
 
   }
 
